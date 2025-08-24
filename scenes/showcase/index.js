a951b27ebc0f42069a991dcda698e785eb61729a
shdewz <shdewz@gmail.com>
2025-08-24 14:42:26 +0300

    Move tetoling code to common

--- scenes/showcase/index.js
+++ scenes/showcase/index.js
@@ -84,23 +84,3 @@ const getModStats = (cs_raw, ar_raw, od_raw, hp_raw, mods) => {
 
 	return { cs, ar, od, hp, ar_ms }
 };
-
-const addTetoFish = (teto) => {
-	const teto_sway_container = $('<img>').attr('src', `../../_shared/assets/tetos/${teto}.gif`).addClass('teto-fish-sway-container');
-
-	const teto_jump_container = $('<div></div>').addClass('teto-fish-jump-container');
-	const teto_move_container = $('<div></div>').addClass('teto-fish-move-container');
-	const animation_duration = 24 + Math.floor(Math.random() * 16);
-	teto_move_container.css('animation-duration', `${animation_duration}s`);
-
-	teto_move_container.append(teto_sway_container);
-	teto_jump_container.append(teto_move_container)
-	$('#teto_fishtank').append(teto_jump_container);
-
-	setInterval(() => {
-		if (Math.random() < 0.02) {
-			teto_jump_container.addClass('teto-jump-activate');
-			setTimeout(() => { teto_jump_container.removeClass('teto-jump-activate'); }, 1000);
-		}
-	}, 1000);
-};
